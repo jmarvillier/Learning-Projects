c9add571c8472e10daec609a30db4e15a62d4cc0
Jeremie Marvillier <JMARVILLIER@cegid.fr>
2019-04-05 12:53:30 +0200

    Books services/storage/authentification
    Init Books project
    
    Authentication
    
    Services and Firebase
    
    Storage

--- Angular/Books - Learning project/src/app/app.component.ts
+++ Angular/Books - Learning project/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'books';
+  title = 'Books App';
+
+  constructor(){
+    const config = {
+      apiKey: "AIzaSyAN3cy6VduzQDOdLeUHxSqsP6jqKuo4xTs",
+      authDomain: "books-9692b.firebaseapp.com",
+      databaseURL: "https://books-9692b.firebaseio.com",
+      projectId: "books-9692b",
+      storageBucket: "books-9692b.appspot.com",
+      messagingSenderId: "546422582269"
+    };
+    
+    firebase.initializeApp(config);
+  }
 }
