0b867067754145e17b05f395ecdac67e67e32872
Jeremie Marvillier <JMARVILLIER@cegid.fr>
2019-04-10 11:33:03 +0200

    firebase

--- Angular/Blog - Exercice project/src/app/app.component.ts
+++ Angular/Blog - Exercice project/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Blog app';
+
+  constructor(){
+    const firebaseConfig = {
+      apiKey: "AIzaSyBV3Cg62S2kIMuD6jyYCE05MVbSFvXyruU",
+      authDomain: "blog-angular-dbc04.firebaseapp.com",
+      databaseURL: "https://blog-angular-dbc04.firebaseio.com",
+      projectId: "blog-angular-dbc04",
+      storageBucket: "blog-angular-dbc04.appspot.com",
+      messagingSenderId: "677766832401"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+  }
 }
--- Angular/Blog - Exercice project/src/app/services/blog-posts.service.ts
+++ Angular/Blog - Exercice project/src/app/services/blog-posts.service.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Post } from '../models/post';
 import { Subject } from 'rxjs';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogPostsService {
 
-  posts: Post[] = [
+  posts: Post[]; /*= [
     { title: 'Create a new angular project', content: 'To create a new angular project, launch the following command: ng new MyProjectName', loveIts: 0, created_at: new Date("2019-04-09:10:10") },
     { title: 'Create a new component', content: 'To create a new angular component, launch the following command: ng generate component MyComponentName', loveIts: 2, created_at: new Date("2019-04-08:11:11") },
     { title: 'Create a service', content: 'To create a new service, launch the following command: ng generate service MyServiceName', loveIts: -2, created_at: new Date("2019-04-07:12:12") }
-  ];
+  ];*/
   postsSubject = new Subject<Post[]>();
+  private dbName = '/blog-posts';
 
-  constructor() { }
+  constructor() { 
+    this.getPosts();
+  }
 
   emitPosts() {
     this.postsSubject.next(this.posts);
@@ -28,8 +32,21 @@ export class BlogPostsService {
     this.postsSubject.next(this.posts);
   }
 
+  getPosts(){
+    firebase.database().ref(`${this.dbName}`)
+    .on('value',(data: firebase.database.DataSnapshot) =>{
+      this.posts = data.val() ? data.val() : [];
+      this.emitPosts();
+    })
+  }
+
+  savePosts(){
+    firebase.database().ref(`${this.dbName}`).set(this.posts);
+  }
+
   createPost(post: Post) {
     this.posts.push(post);
+    this.savePosts();
     this.emitPosts();
   }
 
@@ -42,6 +59,7 @@ export class BlogPostsService {
       }
     );
     this.posts.splice(postToRemove, 1);
+    this.savePosts();
     this.emitPosts();
   }
 
@@ -54,6 +72,7 @@ export class BlogPostsService {
       }
     );
     this.posts[postToUpdate].loveIts = post.loveIts;
+    this.savePosts();
     this.emitPosts();
   }
 }
